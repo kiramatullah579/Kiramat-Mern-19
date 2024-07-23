f79ab78dbbe99a509110a649a6ee2e4866b08321
kiramatullah579 <kiramatk579@gmail.com>
2024-07-23 11:24:21 -0700

    some practious on function has been added

---
+++ function.js
@@ -0,0 +1,55 @@
+function evenOdd(p1, p2) {
+  console.log("p1->", p1);
+  console.log("p2->", p2);
+}
+evenOdd();
+const palandrome = function (p1, p2) {
+  console.log("p1->", p2);
+  console.log("p2->", p1);
+  return true;
+};
+
+function boy() {
+  console.log("my name is khan");
+}
+boy();
+
+function sum(a, b) {
+  console.log(a + b);
+}
+sum(5, 9);
+let greet = function () {
+  console.log("hi good morning");
+};
+greet();
+("arrow function");
+const sayHellow = () => {
+  console.log("hellow");
+};
+sayHellow();
+console.log("<br>");
+const hi = () => {
+  console.log("hi good morning");
+};
+hi();
+var sub = function (p1, p2) {
+  return p1 - p2;
+};
+console.log(sub(6, 3));
+function division(p1, p2) {
+  return p1 / p2;
+}
+console.log(division(6, 4));
+var sub = (p1, p2) => {
+  return p1 - p2;
+};
+console.log(sub(9, 8));
+
+("IIFE");
+(function k() {
+  console.log("hollo");
+})();
+console.log("<br>");
+(() => {
+  console.log("salam friends");
+})();
